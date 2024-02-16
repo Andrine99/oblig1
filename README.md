775046759b72950a704b1ff978b239b5d05c7089
Andrine99 <s360219@oslomet.no>
2024-02-16 14:43:03 +0100

    Opprettelse av Spring Boot-prosjekt og oppdatering av READ ME-fil

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# oblig1
+Oblig 1
+______
+OsloMet-brukernavn: s360219
+GitHub-brukernavn: Andrine99
+GitHub repo URL: https://github.com/Andrine99/oblig1.git
+Fullt navn: Andrine Lærum
+Kort beskrivelse av applikasjonen: 
---
+++ src/main/java/com/example/oblig/Oblig11Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig11Application.class, args);
+    }
+
+}
